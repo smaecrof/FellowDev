74580bbfe55b58a0f331d66a5ab4704a7b55f641
smaecrof <smaecrof@iu.edu>
2020-09-08 15:26:38 -0500

    Switching back to Java
    I have been playing around a bit with python (building games & trying
    Django) but now it is time to go back to Java and work on my skills so
    more. I will be back with python soon enough
    
    09/08/2020

--- fellow_dev/urls.py
+++ fellow_dev/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'', include('fellow_devs.urls', namespace='fellow_devs')),
 ]
